0322f3b80903e11d38b19b93d9b45b1a2761c28e
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2021-10-10 15:37:09 -0300

    version 1: created game: rand.py

---
+++ rand.py
@@ -0,0 +1,24 @@
+import random as t
+
+bot = t.randint(0,1000)
+
+t = 10
+
+print(f"Let's play a game. You have {t} attempts")
+
+a = 0
+
+while True:
+    y = int(input("Type a number: "))
+    a+=1
+
+    if a == t:
+        print("You lose")
+        break
+    if y == bot:
+        print("You win")
+        break
+
+    print("big" if y > bot else "small")
+
+print(f"attempts: {a}")
